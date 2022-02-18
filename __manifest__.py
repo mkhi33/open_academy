be2e8ce0b9e0adeb0daabb28b4ef695d1c98a57c
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-02-18 14:45:09 -0600

    Se crean datos de demostración en el modelo 'course' por medio de las vistas de datos 'course.xml', agregandolas en el __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -24,9 +24,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/course.xml',
+        'views/open_academy_menu.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
