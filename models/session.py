786e95f3d8118db4332f94604ce03499c1ab65ff
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-02-19 11:32:38 -0600

    En el ejercicio 15 se hace uso de campos calculados para obtener el porcentaje de asientos ocupados en una sesión

--- models/session.py
+++ models/session.py
@@ -16,3 +16,12 @@ class open_academy(models.Model):
     instructor_id = fields.Many2one('res.partner', string="Instructor", domain=[('instructor', '=', True),('category_id.name', 'ilike', 'Teacher')])
     course_id = fields.Many2one('open_academy.course', ondelete="cascade", string="Curso", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    taken_seats = fields.Float(string="Taken Seats", compute="_taken_seats" )
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for item in self:
+            if not item.seats:
+                item.taken_seats = 0.0
+            else:
+                item.taken_seats = 100.0 * len(item.attendee_ids) / item.seats
